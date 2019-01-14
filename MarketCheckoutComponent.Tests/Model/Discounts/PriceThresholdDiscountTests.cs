c53115143ec03895b477df5380f61a9ad10c7a51
Krzysztof Kabala <krzykab@gmail.com>
2019-01-14 08:55:14 +0100

    Add price threshold discount rule

---
+++ MarketCheckoutComponent.Tests/Model/Discounts/PriceThresholdDiscountTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using MarketCheckoutComponent.Model.Discounts;
+using MarketCheckoutComponent.Model.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace MarketCheckoutComponent.Tests.Model.Discounts
+{
+	[TestFixture]
+	public class PriceThresholdDiscountTests
+	{
+		[TestCase("Christmas package discount", 600, 10)]
+		[TestCase("Christmas package discount", 1300, 25)]
+		[TestCase("Christmas package discount", 100, 5)]
+		public void Calculate_ReturnsDiscount_WhenTotalProductsPriceIsEqualToThreshold(string discountName, int priceThreshold, int discountPercentage)
+		{
+			//Arrange
+			var packageDiscount = new PriceThresholdDiscount(discountName, priceThreshold, discountPercentage);
+			var productsInTheBasket = GetProducts(priceThreshold);
+
+			//Act
+			var appliedDiscount = packageDiscount.Calculate(productsInTheBasket);
+
+			//Assert
+			appliedDiscount.Should().Be(-(discountPercentage * priceThreshold)/100);
+		}
+
+		[TestCase("Christmas package discount", 600, 10)]
+		[TestCase("Christmas package discount", 1300, 25)]
+		[TestCase("Christmas package discount", 100, 5)]
+		public void Calculate_ReturnsZeroDiscount_WhenTotalPriceIsBelowThreshold(string discountName, int priceThreshold, int discountPercentage)
+		{
+			//Arrange
+			var packageDiscount = new PriceThresholdDiscount(discountName, priceThreshold, discountPercentage);
+			var productsInTheBasket = GetProducts(priceThreshold-1);
+
+			//Act
+			var appliedDiscount = packageDiscount.Calculate(productsInTheBasket);
+
+			//Assert
+			appliedDiscount.Should().Be(0);
+		}
+
+		private IEnumerable<IProduct> GetProducts(int totalPrice)
+		{
+			var products = new List<IProduct>();
+			var moneyLeft = totalPrice;
+
+			var priceGenerator = new Random();
+			while (moneyLeft > 0)
+			{
+				var productMock = new Mock<IProduct>();
+				var price = priceGenerator.Next(1, moneyLeft);
+				productMock.SetupGet(m => m.Price).Returns(price);
+				products.Add(productMock.Object);
+				moneyLeft -= price;
+			}
+
+			return products;
+		}
+
+		[TestCase("Christmas package discount", 600, 10)]
+		[TestCase("Christmas package discount", 1300, 25)]
+		[TestCase("Christmas package discount", 100, 5)]
+		public void Calculate_ReturnsSingleDiscount_WhenDiscountConditionsAreMetMultipleTimes(string discountName, int priceThreshold, int discountPercentage)
+		{
+			//Arrange
+			var priceThresholdDiscount = new PriceThresholdDiscount(discountName, priceThreshold, discountPercentage);
+			var productsInTheBasket = GetProducts(5*priceThreshold).ToList();
+
+			//Act
+			var appliedDiscount = priceThresholdDiscount.Calculate(productsInTheBasket);
+			var productsSum = productsInTheBasket.Sum(m => m.Price);
+
+			//Assert
+			appliedDiscount.Should().Be(-(discountPercentage * productsSum) / 100);
+		}
+
+		[TestCase("Christmas package discount", 600, 10)]
+		[TestCase("Christmas package discount", 1300, 25)]
+		[TestCase("Christmas package discount", 100, 5)]
+		public void Calculate_ReturnsZeroDiscount_WhenNullProductsListIsPassed(string discountName, int priceThreshold, int discountPercentage)
+		{
+			//Arrange
+			var priceThresholdDiscount = new PriceThresholdDiscount(discountName, priceThreshold, discountPercentage);
+
+			//Act
+			var appliedDiscount = priceThresholdDiscount.Calculate(null);
+
+			//Assert
+			appliedDiscount.Should().Be(0);
+		}
+	}
+}
---
+++ MarketCheckoutComponent/Model/Discounts/PriceThresholdDiscount.cs
@@ -0,0 +1,31 @@
+﻿using MarketCheckoutComponent.Model.Discounts.Interfaces;
+using MarketCheckoutComponent.Model.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarketCheckoutComponent.Model.Discounts
+{
+	public class PriceThresholdDiscount : IDiscount
+	{
+		public string Name { get; }
+		private readonly int priceThreshold;
+		private readonly int discountPercentage;
+
+		public PriceThresholdDiscount(string name, int priceThreshold, int discountPercentage)
+		{
+			Name = name;
+			this.priceThreshold = priceThreshold;
+			this.discountPercentage = discountPercentage;
+
+		}
+		public decimal Calculate(IEnumerable<IProduct> products)
+		{
+			if (products == null)
+			{
+				return 0;
+			}
+			var productsSum = products.Sum(m => m.Price);
+			return productsSum >= priceThreshold ? -(discountPercentage * productsSum) / 100 : 0;
+		}
+	}
+}
